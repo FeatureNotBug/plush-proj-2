6ca3d36655b708b1db5621d224f2a208690ac02b
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-07-09 23:34:12 -0400

    username currently works, not liking how recipients is working, though

--- plush-messenger/app/controllers/application_controller.rb
+++ plush-messenger/app/controllers/application_controller.rb
@@ -22,6 +22,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
   #  devise_parameter_sanitizer.for(:sign_up) << :name
   #  devise_parameter_sanitizer.for(:account_update) << :name
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
--- plush-messenger/app/controllers/users_controller.rb
+++ plush-messenger/app/controllers/users_controller.rb
@@ -4,4 +4,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation)
+    end
+  end
 end
