def73c200e09001c33edde6be224670caeb53fcf
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-07-11 13:27:50 -0400

    seems to be able to edit username now!

--- plush-messenger/app/controllers/application_controller.rb
+++ plush-messenger/app/controllers/application_controller.rb
@@ -22,6 +22,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
   #  devise_parameter_sanitizer.for(:sign_up) << :name
   #  devise_parameter_sanitizer.for(:account_update) << :name
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name]) ## add the attributes you want to permit
   end
 end
