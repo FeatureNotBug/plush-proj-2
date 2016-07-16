7d331679a1ab3c3648f819ec2e654e6edae5b936
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-07-15 23:26:28 -0400

    realized problem; never even set confirmation path

--- plush-messenger/config/environment.rb
+++ plush-messenger/config/environment.rb
@@ -4,3 +4,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ActionMailer::Base.smtp_settings = {
+  :user_name => Rails.application.secrets.email_provider_username,#'your_sendgrid_username',
+  :password => Rails.application.secrets.email_provider_password, #'your_sendgrid_password',
+  :domain => 'localhost:3000', #'yourdomain.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
--- plush-messenger/config/environments/development.rb
+++ plush-messenger/config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -33,14 +33,11 @@ Rails.application.configure do
     domain: Rails.application.secrets.domain_name,
     authentication: "plain", 
     user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+    password: Rails.application.secrets.email_provider_password,
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
-  # Send email in development mode?
-  config.action_mailer.perform_deliveries = true
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
