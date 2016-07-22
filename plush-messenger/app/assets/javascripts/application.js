3e873c4c4ecc89834b1f9c5680ad15fff76d21f3
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-07-22 12:02:31 -0400

    ended up using remove turbolinks to fix problem...not happy but it works

--- plush-messenger/app/assets/javascripts/application.js
+++ plush-messenger/app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require chosen-jquery
-//= require turbolinks
+// require turbolinks
 //= require_tree .
