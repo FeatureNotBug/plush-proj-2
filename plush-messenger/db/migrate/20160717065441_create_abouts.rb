12ee850206c5edee90dffec2516b5804080bc4cf
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-07-22 20:17:39 -0400

    fixed up page in case of nil about

--- plush-messenger/db/migrate/20160717065441_create_abouts.rb
+++ plush-messenger/db/migrate/20160717065441_create_abouts.rb
@@ -2,7 +2,7 @@ class CreateAbouts < ActiveRecord::Migration
   def change
     create_table :abouts do |t|
       t.text :aboutme
-      t.text :links
+      t.text :links, array: true, default: []
       t.integer :user_id
     end
   end
